f008c78b0bb5a0de3c1380814783ad1f88092f33
Farad Alam <farad.dev@gmail.com>
2025-08-17 18:41:27 +0600

    user model interface and user model created

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,35 @@
-interface IUser {
-    
+import { Types } from "mongoose";
+
+export enum Role{
+    SUPER_ADMIN = "SUPER_ADMIN",
+    ADMIN = "ADMIN",
+    USER = "USER",
+    GUIDE = "GUIDE"
+}
+
+export enum IsActive {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    BLOCKED = "BLOCKED"
+}
+
+export interface IAuthProvider {
+    provider : string;
+    providerId : string
+}
+
+export interface IUser {
+    name : string;
+    email: string;
+    password ?: string;
+    role : Role;
+    phone ?: string;
+    picture ?: string;
+    address ?: string;
+    isDeleted ?: boolean;
+    isActive ?: IsActive;
+    isVerified ?: boolean;
+    auths : IAuthProvider[];
+    guides ?: Types.ObjectId[]
+    bookings ?: Types.ObjectId[]
 }
