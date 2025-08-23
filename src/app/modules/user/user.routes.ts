b4f0379702b264adfb2eefe9031d765230a6d1ee
Farad Alam <farad.dev@gmail.com>
2025-08-23 22:48:29 +0600

    user validation schema added

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { UserContrller } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 const router = Router();
-router.post("/register", UserContrller.createUser);
+router.post("/register", validateRequest(createUserZodSchema), UserContrller.createUser);
 router.post("/all-user", UserContrller.getAllUser);
 
 const UserRoutes = router;
